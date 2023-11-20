7ecafd54b95f69f595183799ef008d3903737845
Luiza Medeiros bezerra <lumedeir@c1r9p2.42sp.org.br>
2023-11-20 14:04:08 -0300

    minishell

--- main.c
+++ main.c
@@ -6,16 +6,11 @@
 /*   By: lumedeir < lumedeir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 18:23:18 by lde-cast          #+#    #+#             */
-/*   Updated: 2023/11/14 12:16:00 by lumedeir         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:03:12 by lumedeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-typedef enum{
-	APOSTROPHE = 239,
-	DOUBLE_QUOTE = 249
-}SPECIAL;
 
 int	main(void)
 {
